4e7e272bbc948b3d91c6c4cd3908b2ea4f9f3b61
sulan <sulyok.a.attila@gmail.com>
2018-11-16 20:09:49 +0100

    Perform regular test episodes during training

--- train.py
+++ train.py
@@ -25,6 +25,8 @@ VERBOSE_TRAINING = CONFIG.getOption('verbose_training', 0)
 OUTPUT_DATA_FILE = CONFIG.getOption('output_data_file', 'train.out.hdf5')
 # If not None, the Runner will load this file at agent creation
 INPUT_MODEL = CONFIG.getOption('input_model', None)
+# Interval at which test rewards are measured
+TEST_REWARD_INTERVAL = CONFIG.getOption('test_reward_interval', 100)
 
 
 class PrettyPrintEncoder(json.JSONEncoder):
@@ -51,38 +53,51 @@ class TrainingStatisticsLogger(rl.callbacks.Callback):
         - loss (TD-error in one update)
         - the step index at the end of each episode
         - reward at the end of each episode
+        - test reward at regular intervals
         - reward prediction (predicted V value for the starting state after
           each episode)
     """
 
-    def __init__(self, file_name, measurement_name, max_num_steps):
+    def __init__(self, file_name, measurement_name, max_num_steps, env):
         """
         Opens the file for writing (deletes any current content)
         """
         self.file = h5py.File(file_name, 'w')
         self.group = self.file.create_group(measurement_name)
-        self.episode_rewards_size = 8
-        self.num_episodes = self.group.create_dataset('num_episodes',
-                                                      shape = (1,),
-                                                      dtype = 'i8')
-        self.episode_ends = self.group.create_dataset('episode_ends',
-                                                      shape = (self.episode_rewards_size,),
-                                                      maxshape = (None,),
-                                                      dtype = 'i8')
-        self.episode_rewards = self.group.create_dataset('episode_rewards',
-                                                         shape = (self.episode_rewards_size,),
-                                                         maxshape = (None,),
-                                                         dtype = 'f4')
+        self.episode_dataset_capacity = 8
+        self.test_episode_capacity = 8
+        self.num_episodes = self.group.create_dataset(
+            'num_episodes',
+            shape = (1,),
+            dtype = 'i8')
+        self.episode_ends = self.group.create_dataset(
+            'episode_ends',
+            shape = (self.episode_dataset_capacity,),
+            maxshape = (None,),
+            dtype = 'i8')
+        self.episode_rewards = self.group.create_dataset(
+            'episode_rewards',
+            shape = (self.episode_dataset_capacity,),
+            maxshape = (None,),
+            dtype = 'f4')
+        self.test_episode_rewards = self.group.create_dataset(
+            'test_episode_rewards',
+            shape = (self.test_episode_capacity,),
+            maxshape = (None,),
+            dtype = 'f4')
         self.num_steps = 0
         self.percent = 0
         self.max_num_steps = max_num_steps
         self.loss = self.group.create_dataset('loss', shape = (max_num_steps,),
                                               dtype = 'f4')
         self.first_observation = None
-        self.reward_prediction = self.group.create_dataset('reward_prediction',
-                                                           shape = (self.episode_rewards_size,),
-                                                           maxshape = (None,),
-                                                           dtype = 'f4')
+        self.reward_prediction = self.group.create_dataset(
+            'reward_prediction',
+            shape = (self.episode_dataset_capacity,),
+            maxshape = (None,),
+            dtype = 'f4')
+
+        self.env = env
 
     def on_step_end(self, step, logs):
         # TODO find out which one is the loss in a more intelligent manner
@@ -103,11 +118,14 @@ class TrainingStatisticsLogger(rl.callbacks.Callback):
 
     def _grow_episode_datasets(self, episode):
         '''Reallocate array if necessary'''
-        while self.episode_rewards_size <= episode:
-            self.episode_rewards_size *= 2
-        self.episode_rewards.resize(self.episode_rewards_size, axis = 0)
-        self.episode_ends.resize(self.episode_rewards_size, axis = 0)
-        self.reward_prediction.resize(self.episode_rewards_size, axis = 0)
+        while self.episode_dataset_capacity <= episode:
+            self.episode_dataset_capacity *= 2
+        while self.test_episode_capacity <= episode // TEST_REWARD_INTERVAL:
+            self.test_episode_capacity *= 2
+        self.episode_rewards.resize(self.episode_dataset_capacity, axis = 0)
+        self.episode_ends.resize(self.episode_dataset_capacity, axis = 0)
+        self.reward_prediction.resize(self.episode_dataset_capacity, axis = 0)
+        self.test_episode_rewards.resize(self.test_episode_capacity, axis = 0)
 
 
     def on_episode_end(self, episode, logs):
@@ -124,6 +142,19 @@ class TrainingStatisticsLogger(rl.callbacks.Callback):
         self.episode_ends[episode] = self.num_steps
 
         agent = self.model
+        if (episode + 1) % TEST_REWARD_INTERVAL == 0:
+            # Since the test() method has side-effects, we have to save these
+            # two variables and restore them after as a hack
+            agent_training = agent.training
+            agent_step = agent.step
+            history = agent.test(
+                self.env, nb_episodes = 1, visualize = False, verbose = 0,
+                nb_max_episode_steps = 1000)
+            agent.training = agent_training
+            agent.step = agent_step
+            self.test_episode_rewards[episode // TEST_REWARD_INTERVAL] = \
+                history.history['episode_reward']
+
         model = agent.model
         cur_reward_prediction = np.max(model.predict(self.first_observation))
         self.reward_prediction[episode] = cur_reward_prediction
@@ -242,7 +273,7 @@ class Runner(Configurable):
         callbacks = [
             TrainingStatisticsLogger(OUTPUT_DATA_FILE,
                                      self.config['measurement_name'],
-                                     num_steps),
+                                     num_steps, self.env),
             ]
 
         start_time = time.monotonic()
