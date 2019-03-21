90122dedc1927f5b02adc7613369982eb3be589e
sulan <sulyok.andras.attila@itk.ppke.hu>
2019-03-21 17:38:42 +0100

    Add feature to load model in dagger
    Also minor bugfixes

--- imitation/dagger.py
+++ imitation/dagger.py
@@ -2,10 +2,11 @@ import sys
 import numpy as np
 
 from keras.utils import CustomObjectScope
+from keras.models import load_model
 import tqdm
 
 from extract_gameplays import self_play_episode, BOARD_SIZE, CONFIG
-from super import loss, converter2
+from super import loss, converter
 from kaiki_model import create_kaiki_model, GomokuConv
 
 # Number of episodes self-play in each iteration
@@ -14,11 +15,16 @@ NUM_EPISODES_PER_ITERATION = CONFIG.getOption('num_episodes_per_iteration', 100)
 NUM_EPOCHS_PER_ITERATION = CONFIG.getOption('num_epochs_per_iteration', 1000)
 # The name of the output file with the model
 OUTPUT_FILE = CONFIG.getOption('dagger_output_file', 'model.hdf5')
+# Input file name (that contains a previously trained model)
+INPUT_MODEL = CONFIG.getOption('input_model', None)
 
 
 def dagger(num_iterations):
     with CustomObjectScope({'GomokuConv':GomokuConv}):
-        model = create_kaiki_model((2, BOARD_SIZE[0], BOARD_SIZE[1]))
+        if INPUT_MODEL is None:
+            model = create_kaiki_model((2, BOARD_SIZE[0], BOARD_SIZE[1]))
+        else:
+            model = load_model(INPUT_MODEL)
     model.compile(
         optimizer='adam',
         loss=loss,
@@ -30,15 +36,15 @@ def dagger(num_iterations):
         states = []
         expert_actions = []
         for _ in range(NUM_EPISODES_PER_ITERATION):
-            state, _, expert_action= self_play_episode(model)
-            states.append(state)
-            expert_actions.append(expert_action)
+            new_states, _, new_expert_actions = self_play_episode(model)
+            states += new_states
+            expert_actions += new_expert_actions
         states = np.array(states)
-        actions = np.array(actions)
+        expert_actions = np.array(expert_actions)
 
         # Learn
-        states, actions = converter2(states, actions)
-        model.fit(states, actions, batch_size = 2048,
+        states, expert_actions = converter(states, expert_actions)
+        model.fit(states, expert_actions, batch_size = 2048,
                   epochs = NUM_EPOCHS_PER_ITERATION,
                   callbacks = [], verbose = 0, validation_split = 0.8)
 
