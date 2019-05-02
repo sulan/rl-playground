bb97132c667d4f39f7d70beeafaac5fd6b5c8cc9
sulan <sulyok.andras.attila@itk.ppke.hu>
2019-05-02 17:25:37 +0200

    Reorganize RND MNIST experiment code
    - add a new variant where the input of the networks is a previously
    learned feature space

--- misc/network_distillation.py
+++ misc/network_distillation.py
@@ -1,12 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+import tqdm
 
-from keras.models import Sequential
+from keras.models import Sequential, Model
 from keras.layers import Dense, Conv2D, Flatten, MaxPooling2D
 from keras.datasets import mnist
 from keras.utils import to_categorical
 from keras.optimizers import Adam
+from keras.callbacks import EarlyStopping
 
 INPUT_DIM = 2
 OUTPUT_DIM = 2
@@ -15,6 +17,20 @@ N_test = 200
 TRAIN_EPOCHS = 30
 SAME_ARCHITECTURE = True # or a more complex one
 
+#  Dummy tqdm {{{ #
+class tqdm:
+    @classmethod
+    def tqdm(*args, **kwargs):
+        class t:
+            def __init__(self):
+                self.progress = 0
+            def update(self):
+                self.progress += 1
+                print(self.progress)
+        return t()
+#  }}} Dummy tqdm #
+
+#  Init {{{1 #
 # Target network
 activation = 'relu'
 target_model = Sequential([
@@ -55,6 +71,7 @@ def generate_sample(a, b, n):
     r = (b**INPUT_DIM - a**INPUT_DIM) * r + a**INPUT_DIM
     r = r ** (1 / INPUT_DIM)
     return r * surface_points
+#  1}}} #
 
 #  Simple {{{1 #
 sigma = 0.50
@@ -128,69 +145,145 @@ plt.legend(['old before fit', 'old after fit', 'new before fit', 'new after fit'
 
 #  MNSIT experiment (from RND paper) {{{ #
 
+(X, Y), (X_test, Y_test) = mnist.load_data('/tmp/mnist.npz')
+# Add channel count
+X.shape += (1,)
+X_test.shape += (1,)
+
+def train_classifier(classifier):
+    print('Training classifier...')
+    Y_ = to_categorical(Y, 10)
+    classifier.fit(X.reshape((-1, 28, 28, 1)), Y_, epochs = 25000,
+                   validation_split = 0.1,
+                   callbacks = [EarlyStopping(patience = 5)],
+                   verbose = 1)
+    Y_test_ = to_categorical(Y_test, 10)
+    score = classifier.evaluate(
+        X_test.reshape((-1, 28, 28, 1)), Y_test_, verbose = 0)
+    print('Test loss:', score)
+    Y_test_pred = classifier.predict(X_test.reshape((-1, 28, 28, 1)),
+                                     verbose = 0)
+    Y_test_pred = np.argmax(Y_test_pred, axis = -1)
+    print('Test accuracy:', np.mean(Y_test == Y_test_pred))
+
 # Target model
-activation = 'relu'
-target_model = Sequential([
-    Conv2D(20, 3, data_format = 'channels_last', input_shape = (28, 28, 1), activation = activation),
-    Conv2D(20, 3, data_format = 'channels_last', activation = activation),
-    MaxPooling2D(pool_size = (4, 4), data_format = 'channels_last'),
-    Flatten(),
-    Dense(OUTPUT_DIM)
-    ])
-target_model.compile(optimizer = 'sgd', loss = 'mse')
-target_model.summary()
+def gen_target(shared = None):
+    activation = 'relu'
+    if not shared:
+        shared = Sequential([
+            Conv2D(20, 3, data_format = 'channels_last', input_shape = (28, 28, 1),
+                   activation = activation),
+            Conv2D(20, 3, data_format = 'channels_last', activation = activation),
+            MaxPooling2D(pool_size = (4, 4), data_format = 'channels_last'),
+            Flatten(),
+            ])
+        features = shared.output
+        classifier_out = Dense(20, input_shape = features.shape,
+                               activation = activation)(features)
+        classifier_out = Dense(10, input_shape = classifier_out.shape,
+                               activation = 'softmax')(classifier_out)
+        classifier = Model(inputs = shared.input, outputs = classifier_out)
+        classifier.compile(optimizer = Adam(lr = 0.0002),
+                           loss = 'categorical_crossentropy')
+        train_classifier(classifier)
+        for layer in shared.layers:
+            layer.trainable = False
+    features = shared.output
+    target_out = Dense(20, input_shape = features.shape,
+                       activation = activation)(features)
+    target_out = Dense(OUTPUT_DIM, input_shape = target_out.shape,
+                       activation = 'linear')(target_out)
+    target = Model(inputs = shared.input, outputs = target_out)
+    target.compile('sgd', 'mse')
+    return shared, target
 
 # Predictor network
-def gen_predictor():
+def gen_predictor(shared = None):
     activation = 'relu'
-    predictor = Sequential([
-        Conv2D(20, 3, data_format = 'channels_last', input_shape = (28, 28, 1), activation = activation),
-        Conv2D(20, 3, data_format = 'channels_last', activation = activation),
-        MaxPooling2D(pool_size = (4, 4), data_format = 'channels_last'),
-        Flatten(),
-        Dense(OUTPUT_DIM)
-        ])
+    if shared:
+        features = shared.output
+        predictor_out = Dense(20, input_shape = features.shape,
+                              activation = activation)(features)
+        predictor_out = Dense(OUTPUT_DIM, input_shape = predictor_out.shape,
+                              activation = 'softmax')(predictor_out)
+        predictor = Model(inputs = shared.input, outputs = predictor_out)
+    else:
+        predictor = Sequential([
+            Conv2D(20, 3, data_format = 'channels_last',
+                   input_shape = (28, 28, 1), activation = activation),
+            Conv2D(20, 3, data_format = 'channels_last',
+                   activation = activation),
+            MaxPooling2D(pool_size = (4, 4), data_format = 'channels_last'),
+            Flatten(),
+            Dense(20, activation = activation),
+            Dense(OUTPUT_DIM),
+            ])
     predictor.compile(optimizer = Adam(lr = 0.0002), loss = 'mse')
     return predictor
 
-# Dataset
-(X, Y), (X_test, Y_test) = mnist.load_data('/tmp/mnist.npz')
-X_0 = X[Y == 0]
-Y_0 = Y[Y == 0]
-N = 5000
-num_experiments = 10
-num_proportions = 10
-loss = np.zeros((9, num_proportions, num_experiments))
-target_nums = np.logspace(0, np.log10(5000), num_proportions, dtype = 'i')
-for test_no in range(num_experiments):
-    print('Test no.:', test_no)
-    for target_class in range(1, 10):
-        print('Target class:', target_class)
-        X_target = X[Y == target_class]
-        Y_target = Y[Y == target_class]
+def novelty_vs_loss(base_class, target_class, num_experiments = 50,
+                    shared_model = None):
+    num_proportions = 10
+    # 0th axis: shared vs not shared
+    # 2nd axis: target - zero, every other class - zero: min/max
+    loss = np.zeros((2, num_proportions, 3, num_experiments))
+    target_nums = np.logspace(0, np.log10(5000), num_proportions, dtype = 'i')
+    X_base = X[Y == base_class]
+    X_target = X[Y == target_class]
+    other_classes = [i for i in range(10)
+                     if i not in (base_class, target_class)]
+    other_class_inds_test = [(Y_test == i).nonzero() for i in other_classes]
+    progbar = tqdm.tqdm(total = num_experiments * num_proportions * 2,
+                        desc = 'Progress',
+                        bar_format = '{l_bar}{bar}| [{elapsed}<{remaining}{postfix}]')
+    def one_measurement(test_no, shared):
         for i, num_target in enumerate(target_nums):
-            num_0 = N - num_target
-            inds_0 = np.random.choice(X_0.shape[0], size = num_0,
-                                      replace = False)
+            progbar.update()
+            _, target_model = gen_target(shared_model)
+            predictor = gen_predictor(shared_model if shared else None)
+            num_base = 5000
+            inds_base = np.random.choice(X_base.shape[0], size = num_base,
+                                         replace = False)
             inds_target = np.random.choice(X_target.shape[0], size = num_target,
                                            replace = False)
-            train_x = np.r_[X_0[inds_0], X_target[inds_target]]
-            train_x.shape += (1,)
+            train_x = np.r_[X_base[inds_base], X_target[inds_target]]
             train_y = target_model.predict(train_x)
-            predictor = gen_predictor()
             predictor.fit(train_x, train_y, verbose = 0)
 
-            test_x = X_test[Y_test == target_class]
-            test_x.shape += (1,)
-            test_y = target_model.predict(test_x)
-            loss[target_class - 1, i, test_no] = predictor.evaluate(
-                test_x, test_y, verbose = 0)
-plt.plot(target_nums, np.mean(loss, axis = -1).T, '-x')
-plt.legend(np.arange(9) + 1)
-plt.xscale('log')
-plt.xlabel('# of target samples')
+            test_target_vals = target_model.predict(X_test)
+            test_predict_vals = predictor.predict(X_test)
+            scores = (test_target_vals - test_predict_vals)**2
+            score_base = np.mean(scores[Y_test == base_class])
+            score_target = np.mean(scores[Y_test == target_class])
+            score_others = [np.mean(scores[inds])
+                            for inds in other_class_inds_test]
+            loss[int(shared), i, 0, test_no] = score_target - score_base
+            loss[int(shared), i, 1, test_no] = min(score_others) - score_base
+            loss[int(shared), i, 2, test_no] = max(score_others) - score_base
+
+    for test_no in range(num_experiments):
+        one_measurement(test_no, shared = False)
+    for test_no in range(num_experiments):
+        one_measurement(test_no, shared = True)
+
+    return target_nums, loss
+
+shared, _ = gen_target()
+target_nums, loss = novelty_vs_loss(base_class = 0, target_class = 1,
+                                    num_experiments = 20,
+                                    shared_model = shared)
+loss_mean = np.mean(loss, axis = -1)
+plt.figure()
+for i in range(2):
+    plt.subplot(1, 2, i + 1)
+    plt.plot(target_nums, loss_mean[i, :, 0], label = 'loss of target')
+    plt.plot(target_nums, loss_mean[i, :, 1], label = 'loss of others (min)')
+    plt.plot(target_nums, loss_mean[i, :, 2], label = 'loss of others (max)')
+    plt.title(['Not shared', 'Shared'][i])
+    plt.legend()
+    plt.xscale('log')
+    plt.xlabel('# of target samples')
 plt.savefig('loss.pdf')
-#  }}} MNSIT experiment (from RND paper) #
+# }}} MNSIT experiment (from RND paper) #
 
 # vim:set et sw=4 ts=4 fdm=marker:
-
