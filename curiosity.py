424e34ef3c17ffe10c816a523b1e2dd89992196f
sulan <sulyok.andras.attila@itk.ppke.hu>
2019-06-13 21:52:39 +0200

    Add baseline measurement to curiosity.py

--- curiosity.py
+++ curiosity.py
@@ -21,17 +21,18 @@ class CountCuriosityProcessor(rl.core.Processor):
     """
     Count-based exploration via intrinsic rewards
     """
-    def __init__(self):
+    def __init__(self, scale = 1):
         super().__init__()
         self.state_count = Counter()
         self._intrinsic_rewards = []
+        self.scale = scale
 
     def process_step(self, observation, reward, done, info):
         count = self.state_count[tuple(observation.flatten())]
         self.state_count[tuple(observation.flatten())] += 1
         intrinsic_reward = 1 / (1 + count ** 0.5)
         self._intrinsic_rewards.append(intrinsic_reward)
-        reward = reward + intrinsic_reward 
+        reward = reward + self.scale * intrinsic_reward
         return observation, reward, done, info
 
     @property
@@ -55,9 +56,17 @@ def get_model():
     value = Dense(1, activation = 'linear')(shared.output)
     return Model(inputs = shared.inputs, outputs = [policy, value])
 
-def run_count_based():
+def run_count_based(run_baseline = False):
+    """
+    Run PPO with count-based exploration, or optionally without it (this will
+    still count the states for visualisation).
+
+    # Arguments
+    run_baseline (boolean): if true, discards intrinsic reward; also changes
+                            the output names
+    """
     model = get_model()
-    processor = CountCuriosityProcessor()
+    processor = CountCuriosityProcessor(0 if run_baseline else 1)
     fit_epochs = 3
     learner = PPOLearner(model, 10, gamma = 0.99, lam = 0.9,
                          fit_epochs = fit_epochs,
@@ -66,10 +75,11 @@ def run_count_based():
     agent = A2C(learner, num_actors = 32, processor = processor)
     agent.compile(optimizer = Adam(0.0002))
     def env_factory(_):
-        return DumbMars1DEnvironment(20)
-    num_updates = 10000
+        return DumbMars1DEnvironment(30)
+    num_updates = 5000
+    measurement_name = 'baseline' if run_baseline else 'count_based'
     callbacks = [
-        TrainingStatisticsLogger('train.out.hdf5', 'count_based',
+        TrainingStatisticsLogger('train.out.hdf5', measurement_name,
                                  num_updates + 1, env_factory,
                                  loss_per_update = 3 * fit_epochs),
         ]
@@ -78,8 +88,9 @@ def run_count_based():
     print('Test history:')
     print(history.history)
     # Save state counts
-    with open('state.counts', 'wb') as f:
+    with open(measurement_name + '.state.counts', 'wb') as f:
         pickle.dump(processor.state_count, f)
 
 if __name__ == "__main__":
-    run_count_based()
+    run_count_based(True)
+    run_count_based(False)
